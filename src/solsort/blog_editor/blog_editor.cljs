32ac9d863f50b7688c80cf32fbcefc150e66b9cf
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-12-01 18:03:10 +0100

    progress on blog-editor

--- src/solsort/blog_editor/blog_editor.cljs
+++ src/solsort/blog_editor/blog_editor.cljs
@@ -16,20 +16,35 @@
    [clojure.string :as string :refer [replace split blank?]]
    [cljs.core.async :refer [>! <! chan put! take! timeout close! pipe]]))
 
-(db! [:repos] (js/localStorage.getItem "blog-editor-repos"))
+(db! [:repos] (or (js/localStorage.getItem "blog-editor-repos") "rasmuserik/writings"))
 (defn <gh [endpoint]
   (<ajax (str "https://api.github.com/" endpoint "?access_token=" (db [:user :auth "token"]))
                                         ;:headers {:authorization (str "token " )}
-         :credentials false
-         ))
+         :credentials false))
+
+(defn hide-editor! [] (aset js/editorparent.style "height" "0px"))
+(defn show-editor! [] (aset js/editorparent.style "height" "auto"))
+(defn set-editor-content! [s] (js/CKEDITOR.instances.editor.setData s))
+
+(defn load-from-github [file]
+  (go
+    (let [o (clojure.walk/keywordize-keys (<? (<gh (str "repos/" (db [:repos]) "/contents/" (:path file)))))
+          content (js/atob (:content o))
+          [header body] (clojure.string/split content #"\n---\w*\n" 2)
+          header (into {} (map #(clojure.string/split % #":\w*") (rest (clojure.string/split header "\n"))))]
+      (db! [:current]
+           {:header header
+            :body body
+            :sha (:sha o)
+            :path (:path file)})
+      (set-editor-content! body))))
+
 (defn file-info [o]
   {:draft (not (re-find #"^_posts" (o "path")))
    :sha (get o "sha")
    :date (.slice (o "name") 0 10)
-   :file (o "path")
-   :title (.replace (.slice (o "name") 11 -5) (js/RegExp "-" "g") " ")
-   }
-  )
+   :path (o "path")
+   :title (.replace (.slice (o "name") 11 -5) (js/RegExp "-" "g") " ")})
 (let [pos (.indexOf js/location.hash "muBackendLoginToken=")]
   (when (not= -1 pos)
     (go
@@ -50,13 +65,16 @@
         (db!
          [:files]
          (map file-info
-          (filter #(re-find #"[.]html$" (get % "path"))
-                  (concat
-                   (<? (<gh (str "repos/" (db [:repos]) "/contents/_drafts")))
-                   (<? (<gh (str "repos/" (db [:repos]) "/contents/_posts"))))))))
-      (log (db []))
-      )))
+              (filter #(re-find #"[.]html$" (get % "path"))
+                      (concat
+                       (<? (<gh (str "repos/" (db [:repos]) "/contents/_drafts")))
+                       (<? (<gh (str "repos/" (db [:repos]) "/contents/_posts")))))))
+        (load-from-github (first (reverse (sort-by :date (db [:files])))))
+        ))))
+
+
 (defn welcome []
+  (hide-editor!)
   [:div.ui.container
    [:h1 "Blog Editor"]
 
@@ -81,19 +99,39 @@
                             js/location.pathname)}
      "Login to GitHub"]]])
 
-(defn app []
-  [:div.ui.container
-   [:p [:code (db [:repos])] " " [:div.secondary.small.ui.button {:on-click #(js/location.reload)} "Change repository"]]
+(defn file-list []
    (into
-    [:div]
+    [:select
+     {:style {:padding-left 0
+              :padding-top 7
+              :border-radius 5
+              :padding-bottom 7
+              :background :white
+              :padding-right 0}
+      ;:onChange #(db! [:selected-file] (cljs.reader/read-string (.-value (.-target %1))))
+      :onChange #(let [file (cljs.reader/read-string (.-value (.-target %1)))]
+                   (log 'here file)
+                   (load-from-github file)
+                   (db! [:selected-file] file))
+      }
+     ]
     (for [file (reverse (sort-by :date (db [:files])))]
-      [:div.ui.button
-       [:small (:date file) [:br]]
-       (:title file)]
-      ))
-   ])
+      (let [v (prn-str file)]
+        [:option {:style {:padding-left 0
+                          :padding-right 0}
+                  :key v :value v} (str (:date file) " \u00a0 " (:title file))]))
+    ))
+
+(defn app []
+  (show-editor!)
+  [:div.ui.container
+   [:div
+    [file-list] " " [:div.primary.ui.button "new"]
+    [:div {:style {:float :right :display :inline-block}}[:code (db [:repos])] " " [:div.secondary.small.ui.button {:on-click #(js/location.reload)} "Change repository"]]]])
 (defn main []
-  (if (= -1 (.indexOf js/location.hash "muBackendLoginToken"))
-    [welcome]
-    [app]))
+  [:div
+   [:div.ui.red.label "Under development, not functional yet"]
+   (if (= -1 (.indexOf js/location.hash "muBackendLoginToken"))
+     [welcome]
+     [app])])
 (render [main])
